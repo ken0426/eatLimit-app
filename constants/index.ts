b509335bdaed4c45658a4cad229773c518839f28
ken0426 <kenkaitixyou0426@gmail.com>
2022-08-14 00:40:55 +0900

    [feat] カテゴリボタンのオブジェクトを修正＋カテゴリIDを作成

--- constants/index.ts
+++ constants/index.ts
@@ -10,11 +10,12 @@ export const displayOrderDayButton = [
   { buttonName: '日付＋年', option: false },
 ];
 
+/** モーダルのカテゴリ表示で使用するボタン */
 export const displayOrderCategoryButton = [
-  { buttonName: '消費期限', id: 1, right: true, option: false },
-  { buttonName: '賞味期限', id: 2, left: true, option: false },
-  { buttonName: '購入日', id: 3, right: true, option: false },
-  { buttonName: '登録日', id: 4, left: true, option: false },
+  { buttonName: '消費期限', id: 1, right: true },
+  { buttonName: '賞味期限', id: 2, left: true },
+  { buttonName: '購入日', id: 3, right: true },
+  { buttonName: '登録日', id: 4, left: true },
 ];
 
 /** モーダルの画像選択で使用するボタン */
@@ -22,3 +23,11 @@ export const displayOrderIsImageButton = [
   { buttonName: '画像あり', option: true },
   { buttonName: '画像なし', option: false },
 ];
+
+/** カテゴリボタンのID */
+export const CATEGORY_ID = {
+  expiration: 1,
+  expiry: 2,
+  purchase: 3,
+  register: 4,
+};
