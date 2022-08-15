35c58ded6e6cc126bb8584c38fdfebde87bc100b
ken0426 <kenkaitixyou0426@gmail.com>
2022-08-15 23:56:09 +0900

    [fix] jsファイルをtsxに拡張子を変更

---
+++ components/atoms/buttons/DisplayOrderButton.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+
+interface DisplayOrderButtonProps {
+  buttonName: string;
+  key: number;
+  sort: boolean;
+  selectButton: boolean;
+  onPress: () => void;
+  categoryMargin: boolean;
+  right: boolean;
+  left: boolean;
+}
+
+const DisplayOrderButton: React.FC<DisplayOrderButtonProps> = ({
+  buttonName,
+  key,
+  sort,
+  selectButton,
+  onPress,
+  categoryMargin,
+  right,
+  left,
+}) => {
+  return (
+    <View
+      style={{
+        backgroundColor: selectButton ? '#94DFF5' : 'white',
+        borderRadius: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: sort ? 130 : 85,
+        height: 30,
+        borderWidth: !selectButton && 1,
+        borderColor: !selectButton && '#94DFF5',
+        marginBottom: categoryMargin && 20,
+        marginRight: right && 22,
+        marginLeft: left && 22,
+      }}
+    >
+      <TouchableOpacity
+        style={{
+          width: '100%',
+          height: '100%',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+        key={key}
+        onPress={onPress}
+      >
+        <Text
+          style={{
+            color: selectButton ? 'white' : '#94DFF5',
+            fontSize: 15,
+            fontWeight: 'bold',
+          }}
+        >
+          {buttonName}
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default DisplayOrderButton;
