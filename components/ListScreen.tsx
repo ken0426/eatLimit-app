997eacef993139c1f30020d99b840fee1d170339
ken0426 <kenkaitixyou0426@gmail.com>
2022-08-16 21:17:52 +0900

    [fix] リストのボーダ色を変更

--- components/ListScreen.tsx
+++ components/ListScreen.tsx
@@ -96,7 +96,7 @@ const styles = StyleSheet.create({
   box: {
     width: '100%',
     height: 100,
-    borderColor: 'gray',
+    borderColor: '#d3d3d3',
     borderBottomWidth: 0.2,
     borderTopWidth: 0.2,
     padding: 10,
