63e61ba7b009d5d5284fef0a76134d8eac611433
ken0426 <kenkaitixyou0426@gmail.com>
2022-09-04 14:03:44 +0900

    [feat] ラベルカラーを追加

--- styles/index.ts
+++ styles/index.ts
@@ -1,11 +1,23 @@
 export const theme = {
+  /** 中央揃え */
   centerPosition: {
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  /** 縦横最大のサイズ */
   maxSize: {
     width: '100%',
     height: '100%',
   },
+
+  /** テキストのカラー */
   colors: { white: '#fff', rightBlue: '#94DFF5', black: '#000' },
+
+  /** ラベルのカラー
+   * @param blue      冷蔵
+   * @param lightBlue 冷凍
+   * @param lightOrang 常温
+   */
+  labelColors: { blue: '#4169e1', lightBlue: '#afeeee', lightOrang: '#f4a460' },
 };
