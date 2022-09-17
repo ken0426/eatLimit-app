2e77ff3304d2bae49477ba0fcc7aa74e6a6c7fe8
ken0426 <kenkaitixyou0426@gmail.com>
2022-09-18 00:54:13 +0900

    [feat] 詳細画面の実装（仮）

--- components/DetailScreen.tsx
+++ components/DetailScreen.tsx
@@ -1,15 +1,122 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
+import { theme } from '../styles';
 
 interface DetailScreenProps {
-  route: { params: { item: { eatName: string } } };
+  route: {
+    params: { item: { eatName: string; limitDate: string; eatImage: any } };
+  };
 }
 
 const DetailScreen: React.FC<DetailScreenProps> = ({ route }) => {
   const { item } = route.params;
   return (
-    <View style={{ backgroundColor: '#fff', height: '100%' }}>
-      <Text style={{ fontSize: 50 }}>これは{item.eatName}の詳細画面です</Text>
+    <View
+      style={{
+        backgroundColor: theme.backGroundScreenColor.gray,
+        height: '100%',
+        alignItems: 'center',
+        paddingTop: 10,
+        paddingRight: 5,
+        paddingLeft: 5,
+      }}
+    >
+      <View
+        style={{
+          flexDirection: 'row',
+          width: '98%',
+          backgroundColor: theme.colors.white,
+          alignItems: 'center',
+          paddingRight: 15,
+          paddingLeft: 15,
+          marginBottom: 10,
+        }}
+      >
+        <View
+          style={{
+            width: 126,
+            height: 126,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Image
+            style={
+              item.eatImage
+                ? { width: '100%', height: '80%' }
+                : { width: '100%', height: '100%' }
+            }
+            source={
+              item.eatImage ? item.eatImage : require('../images/noImage.png')
+            }
+          />
+        </View>
+        <View style={{ width: '60%', alignItems: 'center' }}>
+          <Text
+            style={{ fontSize: 26 }}
+            numberOfLines={2}
+          >{`${item.eatName}`}</Text>
+        </View>
+      </View>
+      <View
+        style={{
+          width: '98%',
+          backgroundColor: theme.colors.white,
+          flexDirection: 'row',
+          height: 58,
+          paddingRight: 15,
+          paddingLeft: 15,
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          marginBottom: 10,
+        }}
+      >
+        <Text style={{ fontSize: 26 }}>消費期限</Text>
+        <View>
+          <Text style={{ fontSize: 26 }}>{item.limitDate}</Text>
+        </View>
+      </View>
+      <View
+        style={{
+          width: '98%',
+          backgroundColor: theme.colors.white,
+          flexDirection: 'row',
+          height: 58,
+          paddingRight: 15,
+          paddingLeft: 15,
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          marginBottom: 30,
+        }}
+      >
+        <Text style={{ fontSize: 26 }}>保存方法</Text>
+        <View>
+          <Text style={{ fontSize: 26 }}>冷蔵</Text>
+        </View>
+      </View>
+      <TouchableOpacity
+        style={{
+          width: '50%',
+          height: 43,
+          alignItems: 'center',
+          justifyContent: 'center',
+          borderRadius: 50,
+          backgroundColor: theme.colors.rightBlue,
+        }}
+        onPress={() => alert('準備中です')}
+      >
+        <Text
+          style={{
+            textAlign: 'center',
+            justifyContent: 'center',
+            fontSize: 26,
+            color: theme.colors.white,
+            fontWeight: 'bold',
+          }}
+        >
+          消化済み
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
