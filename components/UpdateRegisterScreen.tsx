d0cd688942adcea20f5db4ad0bd0f084d3d7990a
ken0426 <kenkaitixyou0426@gmail.com>
2022-09-19 12:13:08 +0900

    [feat] 編集画面の仮実装

---
+++ components/UpdateRegisterScreen.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const UpdateRegisterScreen = () => {
+  return <Text>UpdateRegisterScreen</Text>;
+};
+
+export default UpdateRegisterScreen;
