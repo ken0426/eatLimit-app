34a5bb8bb26a617f4836280004773af386cb69c1
ken0426 <kenkaitixyou0426@gmail.com>
2022-09-25 12:22:50 +0900

    [fix] Typescriptの方エラーを解消

--- components/RegisterScreen.tsx
+++ components/RegisterScreen.tsx
@@ -16,13 +16,13 @@ import * as ImagePicker from 'expo-image-picker';
 const RegisterScreen = () => {
   const [image, setImage] = useState(null);
   const pickImage = async () => {
-    // No permissions request is necessary for launching the image library
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
+    let result: { uri?: string; cancelled: boolean } =
+      await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
 
     if (!result.cancelled) {
       setImage(result.uri);
