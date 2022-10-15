c7c90c690ab4d185828cf3d34be93d73eb1f91f2
ken0426 <kenkaitixyou0426@gmail.com>
2022-10-16 00:21:11 +0900

    [style] 改行なしのテキストコンポーネントをリファクタリング

--- components/CommonInputText.tsx
+++ components/CommonInputText.tsx
@@ -22,7 +22,7 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   label: {
-    fontFamily: 'HiraginoSans-W3',
+    fontFamily: theme.font.hiragino,
     marginBottom: 10,
     fontSize: 25,
     fontWeight: 'bold',
@@ -40,7 +40,7 @@ const styles = StyleSheet.create({
     height: '100%',
     paddingRight: 8,
     paddingLeft: 8,
-    fontFamily: 'HiraginoSans-W3',
+    fontFamily: theme.font.hiragino,
   },
 });
 
